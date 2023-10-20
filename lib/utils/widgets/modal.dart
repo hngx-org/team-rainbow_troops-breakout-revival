ab5bcdf0036fc0049a85c1b0b5e8f162b23acad8
MrazTevin <millatevin@gmail.com>
2023-10-20 15:17:45 +0300

    fix: add previous button to modal

--- lib/utils/widgets/modal.dart
+++ lib/utils/widgets/modal.dart
@@ -1,14 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-
 class Modal extends StatelessWidget {
   const Modal({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      backgroundColor: const Color(0xff3e4c65) ,
+      backgroundColor: const Color(0xff3e4c65),
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(25),
       ),
@@ -48,26 +47,31 @@ class Modal extends StatelessWidget {
               const Text(
                 "Level 1",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  color: Color(0xFFA9B6CE)
-                ),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFA9B6CE)),
               ),
               const SizedBox(height: 10),
               const Text(
                 "COMPLETE",
                 style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white
-                ),
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
               ),
               const SizedBox(height: 10),
-              SvgPicture.asset(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SvgPicture.asset("assets/images/previous_play_pause.svg", width: 50, height: 50,),
+                  const SizedBox(width: 15,),
+                  SvgPicture.asset(
                     "assets/images/Button_Continue.svg",
                     // width: 125,
                     // height: 125,
                   ),
+                ],
+              )
             ],
           ),
         ),
